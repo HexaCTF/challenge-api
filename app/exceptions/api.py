30c696716eb4a1f674a6454aed2f8944c83e47a4
S0okJu <damekyung98@gmail.com>
2025-01-17 19:54:22 +0900

    [Fix] 잘못된 exception  __init__() 함수명 수정

--- app/exceptions/api.py
+++ app/exceptions/api.py
@@ -11,7 +11,7 @@ class APIException(CustomBaseException):
 class InvalidRequest(APIException):
     """올바르지 않은 요청 예외"""
     def __init__(self, error_msg: str = None):
-        super().__init(
+        super().__init__(
             error_type=ApiErrorTypes.INVALID_REQUEST,
             message="Invalid request format",
             status_code=400,
@@ -21,7 +21,7 @@ class InvalidRequest(APIException):
 class InternalServerError(APIException):
     """서버 내부 오류 예외"""
     def __init__(self, error_msg: str = None):
-        super().__init(
+        super().__init__(
             error_type=ApiErrorTypes.INTERNAL_SERVER_ERROR,
             message="An unexpected error occurred",
             status_code=500,
