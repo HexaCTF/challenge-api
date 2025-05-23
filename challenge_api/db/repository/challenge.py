f4eb3f33a290987fac7a8401a505ff3286970e52
S0okJu <damekyung98@gmail.com>
2025-05-23 14:38:35 +0900

    [Feat] Challenge Repository 생성

--- challenge_api/db/repository/challenge.py
+++ challenge_api/db/repository/challenge.py
@@ -4,12 +4,24 @@ from challenge_api.objects.challenge_info import ChallengeInfo
 from challenge_api.exceptions.api_exceptions import InternalServerError
 from sqlalchemy.exc import SQLAlchemyError
 from typing import Optional
+from challenge_api.db.repository.base import BaseRepository
 
-class ChallengeRepository:
-    
+class ChallengeRepository(BaseRepository):
     def __init__(self, session=None):
-        self.session = session or db.session
+        super().__init__(session)
         
+    def create(self, **kwargs):
+        pass 
+    
+    def get_by_id(self, id_: int) -> Optional[Challenges]:
+        return Challenges.query.get(idx=id_)
+        
+    def update(self, **kwargs):
+        pass 
+    
+    def delete(self, **kwargs):
+        pass 
+    
     def is_exist(self, challenge_id: int) -> bool:
         """
         챌린지 아이디 존재 여부 확인
@@ -18,10 +30,10 @@ class ChallengeRepository:
             challenge_id (int): 챌린지 아이디
         
         """
-        challenge = Challenges.query.get(challenge_id)
-        return challenge is not None
+        return self.get_by_id(id_=challenge_id) is not None 
+        
     
-    def get_challenge_name(challenge_id: int) -> Optional[str]:
+    def get_name(self, challenge_id: int) -> Optional[str]:
         """
         챌린지 아이디로 챌린지 조회
         
@@ -31,6 +43,6 @@ class ChallengeRepository:
         Returns:
             str: 챌린지 이름
         """
-        challenge = Challenges.query.get(challenge_id)
+        challenge = self.get_by_id(id_=challenge_id)
         return challenge.title if challenge else None
     
